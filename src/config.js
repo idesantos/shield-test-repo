90725952218657c672d3498f5d13fcb743b67df3
Ide Santos <57911711+idesantos@users.noreply.github.com>
2026-04-14 22:25:20 -0300

    add hardcoded config

---
+++ src/config.js
@@ -0,0 +1,6 @@
+// config.js
+const supabaseUrl = "https://xyzproject.supabase.co";
+const supabaseServiceRole = "eyJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoic2VydmljZV9yb2xlIn0.fake-service-role-key";
+const adminKey = "minha-admin-key-hardcoded";
+
+export { supabaseUrl, supabaseServiceRole, adminKey };
